981140fb665b01f14a95980e009099392d79a73d
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2024-10-17 05:55:30 +0500

    Update route.ts

--- src/app/api/fetchcredentials/route.ts
+++ src/app/api/fetchcredentials/route.ts
@@ -1,6 +1,8 @@
 import pool from "@/components/utils/db";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const client = await pool.connect();
   try {
