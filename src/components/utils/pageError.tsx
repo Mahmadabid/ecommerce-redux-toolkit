414152a27d627df8af72c85e97642a739dc99fcc
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2024-10-17 06:13:08 +0500

    fixed ui

--- src/components/utils/pageError.tsx
+++ src/components/utils/pageError.tsx
@@ -5,7 +5,7 @@ interface pageErrorProps {
 
 const PageError: React.FC<pageErrorProps> = ({message, messageString}) => {
   return (
-    <div className="flex justify-center items-center min-h-[70vh] text-red-600 text-xl font-semibold">
+    <div className="flex justify-center items-center min-h-[50vh] text-red-600 text-xl font-semibold">
       {message} {messageString}
     </div>
   );
--- src/components/utils/pageLoad.tsx
+++ src/components/utils/pageLoad.tsx
@@ -2,7 +2,7 @@ import Load from "./Load";
 
 const PageLoad = () => {
   return (
-    <div className="flex justify-center items-center min-h-[70vh] text-h-color text-xl font-semibold">
+    <div className="flex justify-center items-center min-h-[50vh] text-h-color text-xl font-semibold">
       Loading&nbsp;
       <Load className="fill-black w-8 h-8" />
     </div>
